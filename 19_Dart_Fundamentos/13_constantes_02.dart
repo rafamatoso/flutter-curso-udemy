eac453cff9698ac5f455f2aa6fe003e90bb4032c
Rafael Matoso <rafaelmatosog@gmail.com>
2021-07-06 20:22:34 -0300

    Constantes #02

---
+++ 19_Dart_Fundamentos/13_constantes_02.dart
@@ -0,0 +1,9 @@
+main() {
+  // Torna o conteúdo da lista imodificável
+  var lista = const ['Rafael', 'Maria', 'Guilherme'];
+
+  lista = const ['Banana', 'Maça'];
+
+  lista.add('Rebeca');
+  print(lista);
+}

0967e6ce3b603099bd1a7616b14ced04f1bfb3b4
Rafael Matoso <rafaelmatosog@gmail.com>
2021-07-07 19:32:16 -0300

    Retornar Função

---
+++ 21_Dart_Funcoes/10_retornar_funcao_02.dart
@@ -0,0 +1,17 @@
+int Function(int) somaDoisTermos(int a) {
+  int c = 0;
+
+  return (int b) {
+    return a + b + c;
+  };
+}
+
+main() {
+  print(somaDoisTermos(2)(10));
+
+  var somaTerceiroTermo = somaDoisTermos(10);
+
+  print(somaTerceiroTermo(3));
+  print(somaTerceiroTermo(7));
+  print(somaTerceiroTermo(19));
+}

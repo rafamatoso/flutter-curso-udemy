3d4f9252f135dba87f2446dd05deaa227473481a
Rafael Matoso <rafaelmatosog@gmail.com>
2021-07-22 20:45:51 -0300

    Arvore de Widgets

--- 02_Flutter_Nocoes_Basicas/projeto1_perguntas/lib/main.dart
+++ 02_Flutter_Nocoes_Basicas/projeto1_perguntas/lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-main() {
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Olá Flutter'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Text('Olá Flutter'),
+      ),
     );
   }
 }

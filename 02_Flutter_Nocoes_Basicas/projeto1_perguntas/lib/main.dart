0f02639669689fa9aa8101ff9ca2c782dc1c915e
Rafael Matoso <rafaelmatosog@gmail.com>
2021-07-22 20:40:13 -0300

    Construindo o Primeiro Widget

---
+++ 02_Flutter_Nocoes_Basicas/projeto1_perguntas/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter'),
+    );
+  }
+}
